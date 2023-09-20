daa33ab3a6366d3bd4f672a8f6e0658a5f1e6cb5
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2023-09-20 17:29:13 -0300

    ✅  SideBar has been created

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        className="h-10 w-10 rounded-full"
+        src="https://github.com/thiagosf-dev.png"
+        alt=""
+      />
+      <div className="flex flex-col truncate">
+        <span className="truncate text-sm font-semibold text-zinc-700">
+          My Full Name
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          myemail@email.com
+        </span>
+      </div>
+      <button className="ml-auto rounded-md p-2 hover:bg-zinc-50" type="button">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,53 @@
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
+import { Logo } from './Logo'
+import { NavItem } from './NavItem'
+import { Profile } from './Profile'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+
+export function Sidebar() {
+  return (
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
+
+      <nav className="space-y-0.5">
+        <NavItem icon={Home} title="Home" />
+        <NavItem icon={BarChart} title="Dashboard" />
+        <NavItem icon={SquareStack} title="Projects" />
+        <NavItem icon={CheckSquare} title="Tasks" />
+        <NavItem icon={Flag} title="Reporting" />
+        <NavItem icon={Users} title="Users" />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem icon={LifeBuoy} title="Support" />
+          <NavItem icon={Cog} title="Settings" />
+        </nav>
+
+        <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200"></div>
+
+        <Profile />
+      </div>
+    </aside>
+  )
+}
